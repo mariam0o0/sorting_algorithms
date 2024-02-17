ebccb3a6baa98e96515ab92d41bc528358a149e6
Mariam <mariam.yasser772000@gmail.com>
2024-02-17 02:16:08 -0800

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,25 @@
 #include "sort.h"
 /**
- * bubble_sort - sort the elements of an array
- * @array: the given array
- * @size: the array size
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
  */
 void bubble_sort(int *array, size_t size)
 {
-	int i, j, temp;
-	bool swapped;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		swapped = false;
-		for (j = 0; j < size - i - 1; j++)
+	size_t i, index, tmp = 0;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+		for (index = 0; index < size; index++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[index] > array[index + 1] && array[index + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				swapped = true;
-				print_array(array, size);
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
 			}
 		}
-		if (swapped == false)
-			break;
-	}
 }
