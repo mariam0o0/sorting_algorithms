8abb3bfc2e15e288e0b4e8602b72ad5331a0a07d
Mariam <mariam.yasser772000@gmail.com>
2024-02-17 02:31:16 -0800

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,24 +7,27 @@
 
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, j, temp;
-        bool swapped;
+	size_t i, j, temp;
+	bool swapped;
 
-        for (i = 0; i < size - 1; i++)
-        {
-                swapped = false;
-                for (j = 0; j < size - i - 1; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                temp = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = temp;
-                                swapped = true;
-                                print_array(array, size);
-                        }
-                }
-                if (swapped == false)
-                        break;
-        }
+	if (!array)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = true;
+				print_array(array, size);
+			}
+		}
+		if (swapped == false)
+			break;
+	}
 }
