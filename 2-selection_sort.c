0ebb9e0599a95d6a4dad891d253abc3f613ca222
Mariam <mariam.yasser772000@gmail.com>
2024-02-17 02:59:30 -0800

    sorting_algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,11 +19,14 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
-			min_idx = j;
+				min_idx = j;
 		}
-		temp = array[i];
-		array[i] = array[min_idx];
-		array[min_idx] = temp;
-		print_array(array, size);
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
 	}
 }
