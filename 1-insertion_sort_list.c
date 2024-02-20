bb741a5bda47ee4e499da3fde59477b5df1a54f1
Mariam <mariam.yasser772000@gmail.com>
2024-02-20 13:58:14 -0800

    sorting

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,21 +1,4 @@
 #include "sort.h"
-/**
-  * print_list - print a linked list
-  * @list: pointer to the list
-  *
-  * Return: Nothing!
-  */
-void print_list(const listint_t *list)
-{
-	const listint_t *current = list;
-
-	while (current != NULL)
-	{
-		printf("%d ", current->n);
-		current = current->next;
-	}
-	printf("\n");
-}
 
 /**
   * insertion_sort_list - Sorts an doubly linked list
